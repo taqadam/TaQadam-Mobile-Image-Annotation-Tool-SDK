9811b9c96d7ef15f1658f29090fd2f2fe2c009e6
Wisso-cmd <guitar90rocker@gmail.com>
2017-11-25 00:04:06 +0300

    Initial commit

--- app/src/main/java/com/recoded/taqadam/MainActivity.java
+++ app/src/main/java/com/recoded/taqadam/MainActivity.java
@@ -1,9 +1,7 @@
 package com.recoded.taqadam;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,11 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.tv).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, TaskActivity.class));
-            }
-        });
     }
 }
