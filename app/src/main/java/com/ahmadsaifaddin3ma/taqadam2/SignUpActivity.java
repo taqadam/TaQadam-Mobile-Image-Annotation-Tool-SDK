fecc65188fa35d29dfb1ae2662faa387452b4582
Ahmad Siafaddin <ahmadsaifaddin43@gmail.com>
2017-11-27 18:01:58 +0300

    draft layouts

--- app/src/main/java/com/ahmadsaifaddin3ma/taqadam2/SignUpActivity.java
+++ app/src/main/java/com/ahmadsaifaddin3ma/taqadam2/SignUpActivity.java
@@ -9,5 +9,7 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //
     }
 }
