77e1acd654ff44e1988a2a7e50a152f81ab01059
ali_abkan <grayeslove@gmail.com>
2017-11-28 15:57:44 +0300

    i made new Activity for payment and finished just the layout works

---
+++ app/src/main/java/com/recoded/taqadam/PaymentActivity.java
@@ -0,0 +1,13 @@
+package com.recoded.taqadam;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class PaymentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+    }
+}
